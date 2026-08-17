bf970a95a00f61d989eb89c79b56e07a26c2ea4d
paucarsn <paucarmovil@gmail.com>
2026-08-17 21:03:52 +0200

    Conexion de la database a postgresql de railway

--- database.py
+++ database.py
@@ -1,9 +1,15 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./peliculas.db"
+DATABASE_URL = os.getenv("DATABASE_URL","sqlite:///./peliculas.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
