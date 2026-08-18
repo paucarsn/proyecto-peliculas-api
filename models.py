cbcf5d6ccc1ebcf788d4a54204ce1140915c65f6
paucarsn <paucarmovil@gmail.com>
2026-08-18 16:14:49 +0200

    modelos de productos

--- models.py
+++ models.py
@@ -1,12 +1,5 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Float
 from database import Base
-
-class PeliculaDB(Base):
-    __tablename__="peliculas"
-
-    id = Column(Integer, primary_key=True, index=True)
-    titulo = Column(String)
-    año = Column(Integer)
 
 class UsuarioDB(Base):
     __tablename__ = "usuarios"
@@ -16,3 +9,37 @@ class UsuarioDB(Base):
     hashed_password = Column(String)
     
     is_superuser = Column(Boolean, default=False)
+
+######################### PRODUCTOS RELATED ################################
+class ProductoDB(Base):
+    __tablename__="productos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String)
+    precio = Column(Float)
+    tipo = Column(String)
+
+    __mapper_args__ = {
+        "polymorpthic_identity": "producto"
+        "polymorpthic_on": tipo
+    }
+
+class LibroDB(ProductoDB):
+    autor = Column(String, nullable=True) 
+    paginas = Column(Integer, nullable=True) 
+    
+    __mapper_args__ = {"polymorphic_identity": "libro"}
+
+class Tecnologia(ProductoDB):
+    marca = Column(String, nullable=True)
+    modelo = Column(String, nullable=True)
+
+    __mapper_args_ = {"polymorphic_identity": "tecnologia"}
+
+class RopaDB(ProductoDB):
+    talla = Column(String, nullable=True)
+    color = Column(String, nullable=True)
+
+    __mapper_args__ = {"polymorphic_identity": "ropa"}
+
+
